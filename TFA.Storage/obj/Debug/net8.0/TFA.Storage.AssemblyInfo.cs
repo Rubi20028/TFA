401512737b5580280ac6f3b52bd6f4396a8d2091
Vlad <LIPADEDO@gmail.com>
2025-03-20 10:39:31 +0900

    Automapper + e2e

--- TFA.Storage/obj/Debug/net8.0/TFA.Storage.AssemblyInfo.cs
+++ TFA.Storage/obj/Debug/net8.0/TFA.Storage.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFA.Storage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4adcf37646a57e4611f4dc2e6187ba1636193e7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e4ad764f289fab70f66a6e1a39330767a17b677")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFA.Storage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFA.Storage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
